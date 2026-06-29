c9234bd1021ada5d995c77fc46378775b5117880
Shreyansh <shreyanshsoni07@gmail.com>
2026-06-29 22:34:03 +0530

    Redeploy original server.js with testing console.logs()

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
-import Fastify from "fastify";
+import "dotenv/config";
+import { buildApp } from "./app.js";
 
-const app = Fastify({
-  logger: true,
-});
+try {
+  console.log("Building app...");
+  const app = await buildApp();
+  console.log("App built successfully.");
 
-app.get("/", async () => {
-  return { message: "Hello Railway!" };
-});
+  app.get("/", async () => {
+    return "Welcome to maic app";
+  });
 
-await app.listen({
-  host: "0.0.0.0",
-  port: process.env.PORT || 3000,
-});
+  await app.listen({
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  });
+
+  console.log("Server listening.");
+} catch (err) {
+  console.error("Startup failed:");
+  console.error(err);
+  process.exit(1);
+}
