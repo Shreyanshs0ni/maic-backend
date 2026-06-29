5d2030e7e356c6da20cfd2a7119f7ac880f31f6d
Shreyansh <shreyanshsoni07@gmail.com>
2026-06-29 22:29:08 +0530

    Try to ix deployment issue, by changing server for once

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
-import "dotenv/config";
-import { buildApp } from "./app.js";
+import Fastify from "fastify";
 
-const app = await buildApp();
-
-app.get("/", (req, res) => {
-  return "Welcome to maic app";
+const app = Fastify({
+  logger: true,
 });
 
-try {
-  await app.listen({
-    port: 3000,
-  });
-} catch (err) {
-  app.log.error(err);
-  process.exit(1);
-}
+app.get("/", async () => {
+  return { message: "Hello Railway!" };
+});
+
+await app.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT || 3000,
+});
